0a6dc8c7ef77f2f64f788a0b7436c83325d726db
Murilo Ramos <okamael.adc@gmail.com>
2022-05-21 12:00:41 -0300

    Criaçã CategoryDTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryDTO implements Serializable{
+	
+	private static final long serialVersionUID = -1351535197279998260L;
+	private Long id;
+	private String name;
+	
+	public CategoryDTO(Category entity) {
+		this.id  =  entity.getId();
+		this.name = entity.getName();
+	}
+	
+}
