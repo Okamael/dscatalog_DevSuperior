41383df717dc6052daa7c2c43bdffdeb0f39f826
Murilo Ramos <okamael.adc@gmail.com>
2022-05-20 21:34:08 -0300

    Configurando acesso ao banco H2 de teste  e criação da service e repository da Category

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,6 +3,12 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 
 import lombok.Getter;
@@ -13,10 +19,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable{
 	
 	private static final long serialVersionUID = 8287813820478712052L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
