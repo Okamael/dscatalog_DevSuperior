1183ce54e2368323dcf126a5cc98d802ae231bca
Murilo Ramos <okamael.adc@gmail.com>
2022-05-24 23:06:11 -0300

    Product repository, service, rresource, GET OK

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO implements Serializable {
+
+	private static final long serialVersionUID = -9035476276250408464L;
+	
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	private Instant date;
+	
+	private List<CategoryDTO> categories = new ArrayList<>();
+	
+	public ProductDTO(Product entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price=entity.getPrice();
+		this.imgUrl=entity.getImgUrl();
+		this.date=entity.getDate();
+	}
+	
+	public ProductDTO(Product entity , Set<Category> categories) {
+		this(entity);
+		categories.forEach(categoria -> this.categories.add(new CategoryDTO(categoria)));
+	}
+
+}
